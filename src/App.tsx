a259ec4b1b36e7eb0cd53532f15c135640cd5172
Pippa <carmela.pippa@hyperisland.se>
2023-12-28 23:06:40 +0100

    add buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
+import { useState } from 'react';
 import './App.css'
+import Alert from './components/Alert';
+import Button from './components/Button';
 import ListGroup from './components/Listgroup'
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   const items = ['New York', "San Francisco", 'Tokyo','London', 'Paris'];
-const handleSelectItem = (item: string)=> {console.log(item)};
+  const handleSelectItem = (item: string)=> {console.log(item)};
   
  return (
  <div>
-  <ListGroup items={items} heading='Cities' onSelectItem={handleSelectItem}>
-    </ListGroup>
+   {alertVisible && <Alert onClose={()=> setAlertVisibility(false)}> Ciao bello! </Alert>}
+   <Button color='danger' onClick={()=> setAlertVisibility(true)}> My Button</Button>
+  <ListGroup items={items} heading='Cities' onSelectItem={handleSelectItem} />
+
     </div>
 )}
 
